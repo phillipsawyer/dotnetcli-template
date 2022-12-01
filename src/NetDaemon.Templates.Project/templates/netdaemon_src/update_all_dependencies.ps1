e45f33244781d862c4ba7dcaa34bae1bc2f38095
Matt Skalecki <matt.skalecki@berrienresa.org>
2022-12-01 13:39:03 -0500

    Fix aliasing issue with 'sort'

--- src/NetDaemon.Templates.Project/templates/netdaemon_src/update_all_dependencies.ps1
+++ src/NetDaemon.Templates.Project/templates/netdaemon_src/update_all_dependencies.ps1
@@ -4,16 +4,16 @@ dotnet tool update joysoftware.netdaemon.hassmodel.codegen
 # Update all nugets to latest versions
 $regex = 'PackageReference Include="([^"]*)" Version="([^"]*)"'
 
-ForEach ($file in get-childitem . -recurse | where { $_.extension -like "*proj" }) {
+ForEach ($file in Get-ChildItem . -Recurse | Where-Object { $_.Extension -like "*proj" }) {
     $packages = Get-Content $file.FullName |
-    select-string -pattern $regex -AllMatches | 
-    ForEach-Object { $_.Matches } | 
-    ForEach-Object { $_.Groups[1].Value.ToString() } | 
-    sort -Unique
+    Select-String -Pattern $regex -AllMatches | 
+        ForEach-Object { $_.Matches } | 
+        ForEach-Object { $_.Groups[1].Value.ToString() } | 
+        Sort-Object -Unique
     
     ForEach ($package in $packages) {
-        write-host "Update $file package :$package"  -foreground 'magenta'
+        Write-Host "Update $file package :$package"  -ForegroundColor Magenta
         $fullName = $file.FullName
-        iex "dotnet add $fullName package $package"
+        Invoke-Expression "dotnet add $fullName package $package"
     }
 }
